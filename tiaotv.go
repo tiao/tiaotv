9731500a71f70f19684fe42045a34de8c1f848cd
Tiao <f.almeida87@gmail.com>
2016-03-03 10:13:57 -0300

    Fix the use of variable conf.http_port
    Other minor fix:
    - Change default port to 8080
    - Remove useless comment
    - Move main to end of file
    
    Signed-off-by: Tiao <f.almeida87@gmail.com>

--- tiaotv.go
+++ tiaotv.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "fmt"
   "html/template"
   "log"
   "net/http"
@@ -16,48 +17,51 @@ type Conf struct {
 }
 
 var conf = Conf {
-  directory: "/mnt/slideshow/SlideEldorado",
-  http_port: 80,
+  directory: "images",
+  http_port: 8080,
   Timeout: 10000,
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+  t := template.New("index")
+  t, err := t.ParseFiles("templates/index.html")
+  if err != nil {
+    panic(err)
+  }
+
+  // Set Files list and refresh timeout
+  conf.Files = getImages()
+  conf.Refresh = (conf.Timeout * len(conf.Files)) / 1000
+  conf.Refresh -= 1
+
+  err = t.Execute(w, conf)
+  if err != nil {
+    panic(err)
+  }
+}
+
+func getImages() []string {
+  files, _ := filepath.Glob(conf.directory + "/*.jpg")
+  for i, p := range files {
+        _, file := filepath.Split(p)
+        // Add prefix /images
+        files[i] = "images/" +file
+  }
+  log.Print(files)
+
+  return files
 }
 
 func main() {
   fs := http.FileServer(http.Dir("."))
   http.Handle("/css/", fs)
   http.Handle("/js/", fs)
+
   fs_images := http.FileServer(http.Dir(conf.directory))
-  // Remove prefix /images
   http.Handle("/images/", http.StripPrefix("/images/", fs_images))
 
   http.HandleFunc("/", serveTemplate)
-
-  log.Println("Listening:8080...")
-  http.ListenAndServe(":8080", nil)
-}
-
-func serveTemplate(w http.ResponseWriter, r *http.Request) {
-  t := template.New("index") // Create a template.
-  t, err := t.ParseFiles("templates/index.html")  // Parse template file.
-  if err != nil { panic(err) }
-
-  // Set refresh timeout
-  conf.Files = getImages()
-  conf.Refresh = (conf.Timeout * len(conf.Files)) / 1000
-  conf.Refresh -= 1
-
-
-  err = t.Execute(w, conf)  // merge.
-  if err != nil { panic(err) }
-}
-
-func getImages() []string {
-  files, _ := filepath.Glob(conf.directory + "/*.['j','J']['p','P']['g','G']")
-  for i, p := range files {
-        _, file := filepath.Split(p)
-        // Add prefix /images
-        files[i] = "images/" +file
-  }
-  log.Print(files) // contains a list of all iamges in the current directory
-
-  return files
+  port := fmt.Sprintf(":%d", conf.http_port)
+  log.Println("Listening " + port)
+  http.ListenAndServe(port, nil)
 }
